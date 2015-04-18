62142c97c5e47647ae192cc6506d7bd8e440e6a1
jordanmather <jordanjmather@gmail.com>
2015-04-17 21:44:06 -0500

    added a mock class to hold the rough methods for grabbing the information needed from our save data

---
+++ InfoCalculations.java
@@ -0,0 +1,10 @@
+
+public class InfoCalculations 
+{
+	/*
+	 * Created by Jordan Mather 9:36 4/17/15
+	 * The contents of this class might end up needing to be split into different classes
+	 * this class will server as a way to outline the methods to be implemented
+	 */
+
+}
